eeed0f4d05aadc8614fac46adda30c65774bce08
Ramkumar C <98400667+RamkumarC0909@users.noreply.github.com>
2025-05-21 21:14:49 +0530

    width and height updates

--- PanelsCreation/BOM.cs
+++ PanelsCreation/BOM.cs
@@ -4,6 +4,8 @@ using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
 using PanelsCreation.AcadComponent;
 using PanelsCreation.DrawSettings;
+using PanelsCreation.Geometry;
+using PanelsCreation.PanelFactory;
 
 namespace PanelsCreation
 {
@@ -12,43 +14,61 @@ namespace PanelsCreation
         [CommandMethod("BOM", CommandFlags.Session)]
         public static void Bom()
         {
-            ObjectIdCollection collection = SelectionSetsHandler.SelectObjectInLayer(LayerSettings.Text);
+            ObjectIdCollection collection = SelectionSetsHandler.SelectObjectInLayer(LayerSettings.Panel);
             using (DocumentLock docLock = AcadCommunication.NewWorkingDocLock)
             {
                 Transaction tr = AcadCommunication.NewWorkingDocTransaction;
 
-                Dictionary<string, int> texts = [];
+                List<(string, PanelDimensions, int)> texts = [];
                 foreach (ObjectId id in collection)
                 {
                     Entity entity = (Entity)tr.GetObject(id, OpenMode.ForRead);
-                    if (entity != null && entity is DBText txt)
+                    if (entity != null && entity is Polyline poly)
                     {
-                        string text = txt.TextString;
-                        if (texts.ContainsKey(text))
+                        Bounds bounds = EntityTransformer.GetBounds(id);
+                        PanelDimensions panelDimensions = new(bounds.Width, bounds.Height);
+                        ObjectIdCollection objectIds = SelectionSetsHandler.SelectObjectWithInBounds(bounds, tr);
+                        if (objectIds.Count == 2) //one for panel one for text
                         {
-                            texts[text] = texts[text] + 1;
-                        }
-                        else
-                        {
-                            texts.Add(text, 1);
+                            Entity ent = (Entity)tr.GetObject(objectIds[0], OpenMode.ForRead);
+                            Entity ent1 = (Entity)tr.GetObject(objectIds[1], OpenMode.ForRead);
+                            DBText? txt = null;
+                            if (ent is DBText) txt = (DBText)ent;
+                            else if (ent1 is DBText) txt = (DBText)ent1;
+                            if (txt != null)
+                            {
+                                string text = txt.TextString;
+                                if (texts.Any(x => x.Item1 == text))
+                                {
+                                    int a = texts.IndexOf(texts.Find(x => x.Item1 == text));
+                                    texts[a] = (texts[a].Item1, texts[a].Item2, texts[a].Item3 + 1);
+                                }
+                                else
+                                {
+                                    texts.Add((text, panelDimensions, 1));
+                                }
+                            }
                         }
                     }
                 }
 
 
                 Table table = new Table();
-                table.SetSize(texts.Count + 1, 3);
+                table.SetSize(texts.Count + 1, 4);
 
                 table.SetTextString(0, 0, "S.No");
                 table.SetTextString(0, 1, "Panel Name");
                 table.SetTextString(0, 2, "Count");
+                table.SetTextString(0, 3, "Width-Height");
                 table.Position = new Point3d(0, 0, 0);
+                table.SetColumnWidth(3);
                 for (int i = 1; i < texts.Count + 1; i++)
                 {
 
                     table.SetTextString(i, 0, i.ToString());
-                    table.SetTextString(i, 1, texts.ElementAt(i - 1).Key);
-                    table.SetTextString(i, 2, texts.ElementAt(i - 1).Value.ToString());
+                    table.SetTextString(i, 1, texts[i - 1].Item1);
+                    table.SetTextString(i, 2, texts[i - 1].Item3.ToString());
+                    table.SetTextString(i, 3, $"{Math.Round(texts[i - 1].Item2.Width, 2)} - {Math.Round(texts[i - 1].Item2.Height, 2)}");
                 }
 
                 AcadCommunication.AppendEntity(table, tr, "0");
