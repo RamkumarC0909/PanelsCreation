4720f262436692e43a807ee747439e697f4cac84
Ramkumar C <98400667+RamkumarC0909@users.noreply.github.com>
2025-05-22 10:55:27 +0530

    Code cleanup

---
+++ PanelsCreation/AcadComponent/TableHandler.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using PanelsCreation.Geometry;
+
+namespace PanelsCreation.AcadComponent
+{
+    public static class TableHandler
+    {
+        public static ObjectId DrawTable(List<List<string>> texts, Point location,
+           Transaction? trOptional = null, string? layer = null)
+        {
+            Transaction tr = trOptional ?? AcadCommunication.NewWorkingDocTransaction;
+
+            Table table = new Table();
+            table.SetSize(texts.Count, 4);
+            table.Position = AcadGeometry.ToPoint3d(location).Value;
+            table.SetColumnWidth(3);
+
+            for (int i = 0; i < texts.Count; i++)
+            {
+                List<string> text = texts[i];
+                for (int j = 0; j < text.Count; j++)
+                {
+                    table.SetTextString(i, j, text[j]);
+                }
+            }
+
+            ObjectId id = AcadCommunication.AppendEntity(table, tr, "0");
+
+            if (trOptional == null) { tr.Commit(); tr.Dispose(); }
+            return id;
+        }
+    }
+}
--- PanelsCreation/BOM.cs
+++ PanelsCreation/BOM.cs
@@ -1,6 +1,5 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
 using PanelsCreation.AcadComponent;
 using PanelsCreation.DrawSettings;
@@ -17,63 +16,56 @@ namespace PanelsCreation
             ObjectIdCollection collection = SelectionSetsHandler.SelectObjectInLayer(LayerSettings.Panel);
             using (DocumentLock docLock = AcadCommunication.NewWorkingDocLock)
             {
-                Transaction tr = AcadCommunication.NewWorkingDocTransaction;
 
-                List<(string, PanelDimensions, int)> texts = [];
-                foreach (ObjectId id in collection)
+                using (Transaction tr = AcadCommunication.NewWorkingDocTransaction)
                 {
-                    Entity entity = (Entity)tr.GetObject(id, OpenMode.ForRead);
-                    if (entity != null && entity is Polyline poly)
+
+                    List<(string, PanelDimensions, int)> texts = [];
+                    foreach (ObjectId id in collection)
                     {
-                        Bounds bounds = EntityTransformer.GetBounds(id);
-                        PanelDimensions panelDimensions = new(bounds.Width, bounds.Height);
-                        ObjectIdCollection objectIds = SelectionSetsHandler.SelectObjectWithInBounds(bounds, tr);
-                        if (objectIds.Count == 2) //one for panel one for text
+                        Entity entity = (Entity)tr.GetObject(id, OpenMode.ForRead);
+                        if (entity != null && entity is Polyline poly)
                         {
-                            Entity ent = (Entity)tr.GetObject(objectIds[0], OpenMode.ForRead);
-                            Entity ent1 = (Entity)tr.GetObject(objectIds[1], OpenMode.ForRead);
-                            DBText? txt = null;
-                            if (ent is DBText) txt = (DBText)ent;
-                            else if (ent1 is DBText) txt = (DBText)ent1;
-                            if (txt != null)
+                            Bounds bounds = EntityTransformer.GetBounds(id);
+                            PanelDimensions panelDimensions = new(bounds.Width, bounds.Height);
+                            ObjectIdCollection objectIds = SelectionSetsHandler.SelectObjectWithInBounds(bounds, tr);
+                            if (objectIds.Count == 2) //one for panel one for text
                             {
-                                string text = txt.TextString;
-                                if (texts.Any(x => x.Item1 == text))
+                                Entity ent = (Entity)tr.GetObject(objectIds[0], OpenMode.ForRead);
+                                Entity ent1 = (Entity)tr.GetObject(objectIds[1], OpenMode.ForRead);
+                                DBText? txt = null;
+                                if (ent is DBText) txt = (DBText)ent;
+                                else if (ent1 is DBText) txt = (DBText)ent1;
+                                if (txt != null)
                                 {
-                                    int a = texts.IndexOf(texts.Find(x => x.Item1 == text));
-                                    texts[a] = (texts[a].Item1, texts[a].Item2, texts[a].Item3 + 1);
-                                }
-                                else
-                                {
-                                    texts.Add((text, panelDimensions, 1));
+                                    string text = txt.TextString;
+                                    if (texts.Any(x => x.Item1 == text))
+                                    {
+                                        int a = texts.IndexOf(texts.Find(x => x.Item1 == text));
+                                        texts[a] = (texts[a].Item1, texts[a].Item2, texts[a].Item3 + 1);
+                                    }
+                                    else
+                                    {
+                                        texts.Add((text, panelDimensions, 1));
+                                    }
                                 }
                             }
                         }
                     }
+
+                    List<List<string>> list = [];
+                    list.Add(["S.No", "Panel Name", "Count", "Width - Height"]);
+                    int i = 1;
+                    foreach (var text in texts)
+                    {
+                        list.Add([i.ToString(), text.Item1, text.Item3.ToString(),
+                        $"{Math.Round(texts[i - 1].Item2.Width, 2)} - {Math.Round(texts[i - 1].Item2.Height, 2)}"]);
+                        i++;
+                    }
+                    TableHandler.DrawTable(list, new(0, 0), tr, "0");
+
+                    tr.Commit();
                 }
-
-
-                Table table = new Table();
-                table.SetSize(texts.Count + 1, 4);
-
-                table.SetTextString(0, 0, "S.No");
-                table.SetTextString(0, 1, "Panel Name");
-                table.SetTextString(0, 2, "Count");
-                table.SetTextString(0, 3, "Width-Height");
-                table.Position = new Point3d(0, 0, 0);
-                table.SetColumnWidth(3);
-                for (int i = 1; i < texts.Count + 1; i++)
-                {
-
-                    table.SetTextString(i, 0, i.ToString());
-                    table.SetTextString(i, 1, texts[i - 1].Item1);
-                    table.SetTextString(i, 2, texts[i - 1].Item3.ToString());
-                    table.SetTextString(i, 3, $"{Math.Round(texts[i - 1].Item2.Width, 2)} - {Math.Round(texts[i - 1].Item2.Height, 2)}");
-                }
-
-                AcadCommunication.AppendEntity(table, tr, "0");
-                tr.Commit();
-                tr.Dispose();
             }
         }
     }
